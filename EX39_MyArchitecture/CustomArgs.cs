6dbffb0c52155f06c7f3ea676a27d0696d35c993
Emil Klinggaard Holm Hansen <emil129e@edu.eal.dk>
2018-12-19 11:51:42 +0100

    Add project files.

---
+++ EX39_MyArchitecture/CustomArgs.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelViewPresenter01
+{
+    public class CustomArgs : EventArgs
+    {
+		//implement as described in the class diagram
+		
+        public CustomArgs(string before, string after)
+        {
+            MessageAfter = after;
+            MessageBefore = before;
+
+            //to be implemented
+        }
+
+        public string MessageAfter { get; internal set; }
+        public string MessageBefore { get; internal set; }
+    }
+}
---
+++ EX39_MyArchitecture/Screen.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelViewPresenter01
+{
+    public class Screen : IScreen
+    {
+        public string Answer { get; set; }
+        public string Warning { get; set; }
+        public string TextValue { get; set; }
+
+        private readonly string exitString = "q";
+        public Worker MyWorker { get; set; }
+
+        public Screen(IMessage message)
+        {
+            TextValue = message.MyMessage;
+            Warning = "Nothing to warn about";
+
+            message.MessageSetEvent += MessageSetEventHandler;
+
+            MyWorker = new Worker(this, message);           
+
+            //Screen needs to add its MessageSetEventHandler method
+            //to the event in message
+
+            //Also Screen needs to create its worker instance
+        }
+
+        void MessageSetEventHandler (object sender, CustomArgs customArgs)
+        {
+			//sender is not used as all information needed is in customArgs
+			
+			TextValue = customArgs.MessageAfter;
+            Warning = $"Text changed from: \"{customArgs.MessageBefore}\"";
+
+        }
+
+        public void ShowScreen()
+        {
+            bool stop;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("*************************************");
+                Console.WriteLine($"Warning : {Warning}");
+                Console.WriteLine($"Text : {TextValue}\n");
+                Console.WriteLine("*************************************\n");
+                Console.WriteLine($"Enter \"{exitString}\" to exit program");
+                Console.WriteLine($"Enter \"r\" to reverse \"Text\"");
+                Console.WriteLine($"Any other text changes \"Text\"\n");
+                Console.Write("Enter choice: ");
+                Answer = Console.ReadLine();
+                if (Answer.ToUpper().Equals("R"))
+                {
+
+                    MyWorker.ReverseTextValue();
+
+                    //The Message.MyMessage should be changed to the reverse
+                    //If it was "Hello mom" it should be "mom olleH"
+                } else
+                {
+                    //The Message.MyMessage should change to the text in Answer
+                    MyWorker.SetTextValue();
+                }
+                stop = Answer.ToUpper().Equals(exitString.ToUpper());
+            } while (stop == false);
+        }
+    }
+}
